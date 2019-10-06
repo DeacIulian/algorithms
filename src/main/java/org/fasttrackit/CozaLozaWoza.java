65803cecfd921a84df3fba46253be741bccf624f
Deac Iulian <deaciulian91@gmail.com>
2019-10-07 01:16:04 +0300

    solved CozaLozaWoza with method

--- src/main/java/org/fasttrackit/CozaLozaWoza.java
+++ src/main/java/org/fasttrackit/CozaLozaWoza.java
@@ -1,24 +1,32 @@
 package org.fasttrackit;
 
+import java.util.Scanner;
+
 public class CozaLozaWoza {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 110; i++) {
+        CozaLozaWoza cozaLozaWoza = new CozaLozaWoza();
+        cozaLozaWoza.printCozaLozaWoza(34,150);
+    }
+
+    public void printCozaLozaWoza(int start, int end){
+        for (int i = start; i <= end; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print(" CozaLoza ");
+                System.out.print("CozaLoza");
             } else if (i % 3 == 0) {
-                System.out.print(" Coza ");
+                System.out.print("Coza");
             } else if (i % 5 == 0) {
-                System.out.print(" Loza ");
+                System.out.print("Loza");
             } else if (i % 7 == 0) {
-                System.out.print(" Woza ");
+                System.out.print("Woza");
             } else {
-                System.out.print(" " + i + " " );
+                System.out.print(i);
             }
             if (i % 11 == 0) {
                 System.out.println();
+            } else {
+                System.out.print(" ");
             }
         }
-
     }
 }
